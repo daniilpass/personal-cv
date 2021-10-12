c2dfc9ea577f4130487008b9e2dd4fcb37b4d467
Daniil <daniil@pas>
2021-10-12 22:04:45 +0300

    Initial commit

---
+++ web/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "app/js/bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      minify: false
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./app/css/[name].css"
+    }),
+  ],
+};
