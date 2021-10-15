e74ba97b1906969106639719a48f533b54109a26
Daniil <daniil@pas>
2021-10-16 00:44:38 +0300

    intro text upd

--- web/src/components/templates/intro/Intro.jsx
+++ web/src/components/templates/intro/Intro.jsx
@@ -27,7 +27,7 @@ export function Intro(props) {
             <div className="intro__about">
                 I have strong skills in object-oriented programming, knowledge of design patterns and data structures. {br}
                 Moreover, I have experience in parallel and asynchronous programming. {br}
-                Familiar with the web architecture and development. {br}
+                Familiar with the web development. {br}
             </div>
 
             <div className="intro__contacts">
