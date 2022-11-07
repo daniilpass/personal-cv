9361cf084f3419f5f0105c710443845dd0a5de26
Daniil <anil1641@mail.ru>
2022-11-07 16:33:23 +0300

    export import logos

---
+++ web/src/assets/logos.js
@@ -0,0 +1,23 @@
+import BitbucketLogo from "../assets/img/bitbucket.png";
+import TelegramLogo from "../assets/img/telegram.png"
+import LinkedinLogo from "../assets/img/linkedin.png"
+import BeatLogo from "../assets/img/beat.png";
+import AsciiLogo from "../assets/img/ascii.png";
+import KashaLogo from "../assets/img/kasha.png";
+import VtbLogo from "../assets/img/vtb.png";
+import GpbLogo from "../assets/img/gpb.png";
+import VoliLogo from "../assets/img/voli.jpeg";
+import YandexLogo from "../assets/img/yandex.png";
+
+export  {
+    BitbucketLogo,
+    TelegramLogo,
+    LinkedinLogo,
+    BeatLogo,
+    AsciiLogo,
+    KashaLogo,
+    VtbLogo,
+    GpbLogo,
+    VoliLogo,
+    YandexLogo
+}
--- web/src/settings/i18n/resume/ru.js
+++ web/src/settings/i18n/resume/ru.js
@@ -1,14 +1,16 @@
 import React from "react";
-import BitbucketLogo from "../../../assets/img/bitbucket.png";
-import TelegramLogo from "../../../assets/img/telegram.png"
-import LinkedinLogo from "../../../assets/img/linkedin.png"
-import BeatLogo from "../../../assets/img/beat.png";
-import AsciiLogo from "../../../assets/img/ascii.png";
-import KashaLogo from "../../../assets/img/kasha.png";
-import VtbLogo from "../../../assets/img/vtb.png";
-import GpbLogo from "../../../assets/img/gpb.png";
-import VoliLogo from "../../../assets/img/voli.jpeg";
-import YandexLogo from "../../../assets/img/yandex.png";
+import {
+    BitbucketLogo,
+    TelegramLogo,
+    LinkedinLogo,
+    BeatLogo,
+    AsciiLogo,
+    KashaLogo,
+    VtbLogo,
+    GpbLogo,
+    VoliLogo,
+    YandexLogo
+} from "../../../assets/logos";
 
 export const ru = {
     aboutMe: {
